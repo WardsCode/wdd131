aedbe651ab658ae0c501ce5e7685613948c6c5f9
WardsCode <wardsnotes7@gmail.com>
2024-06-22 07:46:05 -0500

    added

--- scripts/temples.js
+++ scripts/temples.js
@@ -14,13 +14,11 @@ lastm.innerHTML = `<span class="highlight">${date}</span>`;
 
 /**/
 
-const mainnav = document.querySelector('.navigation');
+const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
 
-
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
 hambutton.addEventListener('click', () => {
-
-    mainnav.classList.toggle('show');
-    hambutton.classList.toggle('show');
-
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
 });
