ee4de06367c377548c3f4210ae18f67b63eff23e
WardsCode <wardsnotes7@gmail.com>
2024-06-29 12:48:19 -0500

    added

--- scripts/place.js
+++ scripts/place.js
@@ -9,3 +9,21 @@ lastm.innerHTML = `<span class="highlight">${date}</span>`;
 
 /**/
 
+
+let temperature = 20;
+let windspeed = 6;
+const chill = document.querySelector("#windchill");
+
+if (temperature <= 50 && windspeed > 3 ){
+
+    chill.innerHTML = `<span class="windchill"> ${calculateWindChill(temperature,windspeed)}</span>`;
+    function calculateWindChill(temperature,windspeed){
+            return (35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windspeed , 0.16)) + (0.4275 * temperature * Math.pow(windspeed,0.16))).toFixed(2);
+    }
+} else {
+    chill.innerHTML = "No Valid";
+}
+
+
+
+
